a7cb51f4c6c600157c35af0f02cae90272053a96
timbog <timbog@mail.ru>
2014-04-06 17:17:11 +0400

    some test files added

---
+++ ClientWCFNET/ClientWCFNET/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+using System.Web;
+namespace ClientWCFNET
+{
+    class Client
+    {
+        public IService service {get; private set;}
+
+        ChannelFactory<IService> cf;
+
+        public Client(string str)
+        {
+            cf = new ChannelFactory<IService>(new WebHttpBinding(), str);
+            cf.Endpoint.Behaviors.Add(new WebHttpBehavior());
+            service = cf.CreateChannel();
+        }
+
+    }
+}
---
+++ ClientWCFNET/ClientWCFNET/IService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+using System.Web;
+namespace ClientWCFNET
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebGet]
+        string EchoWithGet(string s);
+
+        [OperationContract]
+        [WebInvoke]
+        string EchoWithPost(string s);
+    }
+}
